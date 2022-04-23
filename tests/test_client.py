ca8ed16fb4e51fc5873e2cae840fef2e35320df7
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-04-24 01:13:43 +0200

    added tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,7 @@
 from random import randint, sample
 from requests.exceptions import HTTPError
 from tests.test import ClockifyTestCase
-from clockify.client.client_dto import ClientDTO
+from clockify.client.client_dto import ClientDTO, ClientQueryDTO, SortColumn, SortOrder
 from clockify.session import ClockifySession
 
 
@@ -28,6 +28,25 @@ class TestClients(ClockifyTestCase):
 
     def test_get_list_of_clients_fail(self):
         self.assertRaises(HTTPError, self.session.get_clients, "fakeworkspace")
+
+    def test_query_name_ascending(self):
+        query = ClientQueryDTO(False, "", 1, 50, SortColumn.NAME, SortOrder.ASCENDING)
+        clients = self.session.get_clients(self.WORKSPACE, query)
+        for client in clients:
+            self.assertIsInstance(client, ClientDTO)
+
+    def test_query_id_descending(self):
+        query = ClientQueryDTO(True, "", 1, 200, SortColumn.ID, SortOrder.DESCENDING)
+        clients = self.session.get_clients(self.WORKSPACE, query)
+        for client in clients:
+            self.assertIsInstance(client, ClientDTO)
+
+    def test_query_fail(self):
+        query = ClientQueryDTO(sort_column="FAKE")
+        self.assertRaises(HTTPError, self.session.get_clients, self.WORKSPACE, query)
+
+        query = ClientQueryDTO(sort_order="UPSIDEDOWN")
+        self.assertRaises(HTTPError, self.session.get_clients, self.WORKSPACE, query)
 
     def test_get_client_by_id(self):
         client = ClientDTO(f"Test Get {randint(0, 99999)}")
